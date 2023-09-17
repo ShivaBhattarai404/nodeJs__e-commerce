fae312c1a43894a0332e637e7f079f5621d7a6d5
Shiva <shivabhattarai15@gmail.com>
2023-09-17 21:28:46 +0545

    create a basic server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const { mongoose } = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.listen(3000);
